916e92e0ab822ae5981026a124fbeca1f42ed093
codinci <dkibau@gmail.com>
2021-12-26 01:59:32 +0300

    Resolving 'power' to not required

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -3,7 +3,7 @@ export class Hero {
     constructor(
         public id: number,
         public name: string,
-        public power: string,
+        public power?: string,
         public alterEgo?: string
     ) { }
 }
